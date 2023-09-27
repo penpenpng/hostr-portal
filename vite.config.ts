ef76dcddea59602016a8daeb6dbc842d39249e32
penpenpng <poman1638m@gmail.com>
2023-09-27 23:59:41 +0900

    fix: base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "hostr-portal",
   plugins: [svelte()],
   build: {
     target: "esnext",
